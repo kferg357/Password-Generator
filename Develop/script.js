a3287aed94b9283d29d22b724031702405e41b06
kferg357 <kennethferguson90@gmail.com>
2021-04-14 09:10:43 -0400

    new info

--- Develop/script.js
+++ Develop/script.js
@@ -12,3 +12,50 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+ //some way for a user to input the desired length of the password
+
+var num = prompt('How many characters do you want')
+console.log(num)
+
+
+//hello
+
+var lowerCaseLetters = 'asdked'
+var numbers = '1234567789'
+var selectedCharacters = ''
+console.log
+//verify the user is typing in a valid value (numbers between 0-128)
+
+
+
+// some way for the user to select the type of characters they t
+var lowercase = confrim('Do you want to include lowercase letters')
+//find a wat to check what the users input was we also want to do something with the info
+
+if(lowercase === true){
+  selectedCharacters = selectedCharacters + lowerCaseLetters 
+  console.log(selectedCharacters)
+console.log('the user picked true')
+} else {
+console.log('The user picked false')
+}
+
+if(lowercase === true){
+  selectedCharacters = selectedCharacters + numbers
+  console.log(selectedCharacters)
+
+}
+
+...// select a raondom value from selected characters
+  
+}
+var string
+
+for (var i=0; i< i++) var0++) {
+  //select a random value
+  console.log(i)
+  
+}
+
+
+//find a way to reconcile the number of characters chosen with what they want
