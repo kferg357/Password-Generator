e25c66e64a9c9e612d7a78cbc4ff2b3614d36f64
kferg357 <kennethferguson90@gmail.com>
2021-04-17 18:48:31 -0400

    mdified js and deleted notes

--- script.js
+++ script.js
@@ -79,46 +79,10 @@ var selectedCharacters = ''
   for (var i = 0; i < num; i++) {
     var characterIndex = Math.floor(Math.random()*selectedCharacters.length);
     var character = selectedCharacters[characterIndex];
-      // selectedChatacters = "abcdefghij...."
-      // selectedCharacters[2] = c;
-      // selectedCharacters = [1: hello, 2: "bye"]
-      //selectedCharacters.2 = "bye"
-
-
+   
 
     password = password + character
   }
-
-
-  
-  // var lowerCaseLetters = 'asdked' 
-  // var numbers = '1234567789'
-  // var selectedCharacters = ''
-  // console.log
-  // //verify the user is typing in a valid value (numbers between 0-128)
-  // 
-  // console.log(num)
-
-  // // some way for the user to select the type of characters they t
-  // var lowercase = confrim('Do you want to include lowercase letters')
-  // //find a wat to check what the users input was we also want to do something with the info
-
-  // if(lowercase === true){
-  //   selectedCharacters = selectedCharacters + lowerCaseLetters 
-  //   console.log(selectedCharacters)
-  // console.log('the user picked true')
-  // } else {
-  // console.log('The user picked false')
-  // }
-
-  // if(lowercase === true){
-  //   selectedCharacters = selectedCharacters + numbers
-  //   console.log(selectedCharacters)
-
-  // }
-
   //returnpasswored value 
   return password
 }
-
-//find a way to reconcile the number of characters chosen with what they want
